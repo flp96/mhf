c9c0a80a17c4723908aecc094a264c1c3fc39695
flp96 <filipefernandes__@live.com>
2024-10-01 13:10:45 -0300

    accordion list js finalizado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 import AnimaScroll from './modules/anima-scroll.js';
+import Accordion from './modules/accordion.js';
 
 const animaScroll = new AnimaScroll('[data-anime="scroll"]');
 animaScroll.init();
+
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+

a450e07cc7761ce870262efe88373d22d1b800f4
flp96 <filipefernandes__@live.com>
2024-10-03 13:39:30 -0300

    esqueleto dos eventos do slide adicionados

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import AnimaScroll from './modules/anima-scroll.js';
 import Accordion from './modules/accordion.js';
+import Slide from './modules/slide.js';
+
+const slide = new Slide('.slide', '.slide-wrapper');
+slide.init();
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
